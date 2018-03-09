2859c1eec3345f029147fea7465d5aba20d141af
MrLumi <lumilesff@hotmail.com>
2018-03-09 20:42:34 +0000

    #fix

--- esx_truck_inventory.sql
+++ esx_truck_inventory.sql
@@ -4,8 +4,7 @@ CREATE TABLE IF NOT EXISTS `truck_inventory` (
   `count` int(11) NOT NULL,
   `plate` varchar(8) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `itemt` varchar(100) NOT NULL,
-  `owned` varchar(255) NOT NULL
+  `owned` varchar(255) NOT NUL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 ALTER TABLE `truck_inventory`
