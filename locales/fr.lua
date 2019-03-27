b54b5ad6f0b417c1550c414ff1da26021c677bb6
BackrndSource <back.rndsource@gmail.com>
2019-03-27 21:25:08 +0000

    Create fr.lua

---
+++ locales/fr.lua
@@ -0,0 +1,12 @@
+Locales['fr'] = {
+	['coffre_ferme'] = 'ce coffre est ~r~fermé',
+	['pas_vehicule'] = 'pas de ~r~véhicule~w~ à proximité',
+	['deposer'] = 'déposer',
+	['contenu_coffre'] = 'contenu du coffre',
+	['contenu_inventaire'] = 'contenu de l\'inventaire',
+	['quantite'] = 'quantité',
+	['poid_coffre'] = 'poid du coffre :',
+	['atteint_limite'] = 'vous avez atteint la limite des',
+	['quantite_invalide'] = '~r~ quantité invalide',
+	['porte_trops'] = '~r~ tu en porte trops',
+}
