1755dd25308cfbf8367dc0e97990f31553963183
BackrndSource <back.rndsource@gmail.com>
2019-03-27 21:32:43 +0000

    Create es.lua

---
+++ locales/es.lua
@@ -0,0 +1,12 @@
+Locales['es'] = {
+	['coffre_ferme'] = 'este maletero está ~r~cerrado',
+	['pas_vehicule'] = 'no estás ~r~cerca~w~ de ningún vehículo',
+	['deposer'] = 'dejar',
+	['contenu_coffre'] = 'contenido del maletero',
+	['contenu_inventaire'] = 'en inventario',
+	['quantite'] = 'cantidad',
+	['poid_coffre'] = 'peso del maletero :',
+	['atteint_limite'] = 'has superado el límite de',
+	['quantite_invalide'] = '~r~Cantidad no válida',
+	['porte_trops'] = '~r~ Llevas demasiado...',
+}
