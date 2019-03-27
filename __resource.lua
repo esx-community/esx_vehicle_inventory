a1d180616488e1cfb68d80d2bd2d2d5abb7718c6
BackrndSource <back.rndsource@gmail.com>
2019-03-27 21:34:11 +0000

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -7,11 +7,21 @@ version '1.0.0'
 server_scripts {
   '@async/async.lua',
   '@mysql-async/lib/MySQL.lua',
+  '@es_extended/locale.lua',
+  'locales/es.lua',
+  'locales/fr.lua',
   'config.lua',
   'server/main.lua'
 }
 
 client_scripts {
+  '@es_extended/locale.lua',
+  'locales/es.lua',
+  'locales/fr.lua',
   'config.lua',
   'client/main.lua'
+}
+
+dependencies {
+	'es_extended'
 }
