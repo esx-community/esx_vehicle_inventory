e7213ce0d0c505719b5a13bb4c4bc6bbc06326dc
BackrndSource <back.rndsource@gmail.com>
2019-03-27 21:33:26 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,19 +1,17 @@
---Truck
-Config	=	{}
+Config	                = {}
+Config.Locale           = 'es'
 
  -- Limit, unit can be whatever you want. Originally grams (as average people can hold 25kg)
-Config.Limit = 25000
+Config.Limit            = 25000
 
 -- Default weight for an item:
 	-- weight == 0 : The item do not affect character inventory weight
 	-- weight > 0 : The item cost place on inventory
 	-- weight < 0 : The item add place on inventory. Smart people will love it.
-Config.DefaultWeight = 0
-
-
+Config.DefaultWeight    = 0
 
 -- If true, ignore rest of file
-Config.WeightSqlBased = false
+Config.WeightSqlBased   = false
 
 -- I Prefer to edit weight on the config.lua and I have switched Config.WeightSqlBased to false:
 
@@ -44,5 +42,4 @@ Config.VehicleLimit ={
     [18]=50000,
     [19]=50000,
     [20]=150000,
-
 }
